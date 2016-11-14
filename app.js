07aa0dbcfa846589265aae2cdb2d5897f4c2415b
Thomas Swann <tkswann@mac.com>
2016-11-14 15:20:16 -0600

    added setBackground to a setInterval, added a clearInterval function call

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 'use strict'
 const body = document.body
+
 // string interpolation/templating to incorporate the '#' symbol
 // returns a random float between 0 and 1
 // toString(16) converts that return to base16 aka HEX
@@ -11,9 +12,12 @@ const setBackground = (color) => {
   body.style.background = color
 }
 
-body.addEventListener('keypress', (e) => {
-  if(e.keyCode === 32) {
-    setBackground(getColor())
-  }
-  return null
-})
+// body.addEventListener('keypress', (e) => {
+//   if(e.keyCode === 32) {
+//     setBackground(getColor())
+//   }
+//   return null
+// })
+
+const timeout = setInterval(() => setBackground(getColor()), 250)
+const clearThings = () => clearInterval(timeout)
