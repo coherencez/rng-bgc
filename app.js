f29eb1862b63847c564428d1e9282c48c0ac82f6
Thomas Swann <tkswann@mac.com>
2016-11-14 19:34:36 -0600

    the road to cellular automata begins

--- app.js
+++ app.js
@@ -7,22 +7,23 @@ const     boxes = document.getElementsByClassName('box')
 // returns a random float between 0 and 1
 // toString(16) converts that return to base16 aka HEX
 // slice(2,8) removes the 0. at the start, and returns the next 6 chars
-const getColor = () => `#${Math.random().toString(16).slice(2,8)}`
+// const generateRandomColor = () => `#${Math.random().toString(16).slice(2,8)}`
+const generateRandomColor = () => {
+  const oneOrZero = Math.round(Math.random())
+  let color = null
+  ;(oneOrZero === 1)
+    ? color = '#000'
+    : color = '#fff'
+  return color
+}
 
-// set background of body element to the color passed to function
+// set background of element to the color passed to function
 const setBackground = (element, color) => {
   element.style.background = color
 }
 
-// body.addEventListener('keypress', (e) => {
-//   if(e.keyCode === 32) {
-//     setBackground(getColor())
-//   }
-//   return null
-// })
-
 // create boxes in the DOM
-for (let i = 0; i < 100; i++) {
+for (let i = 0; i < 15000; i++) {
   const box = document.createElement('div')
         box.classList.add('box')
         container.appendChild(box)
@@ -31,16 +32,35 @@ for (let i = 0; i < 100; i++) {
 const startThings = () => {
   const interval = 250
 
-  const colorChangeInterval2 = setInterval(() => {
+  let timer1 //= setInterval(() => setBackground(body, generateRandomColor()), interval)
+
+  const timer2 = setInterval(() => {
     for (let i = 0; i < boxes.length; i++) {
-      setBackground(boxes[i], getColor())
+      setBackground(boxes[i], generateRandomColor())
     }
   }, interval)
 
-  const colorChangeInterval = setInterval(() => setBackground(body, getColor()), interval)
-  return { colorChangeInterval, colorChangeInterval2 }
+
+  return { timer1, timer2 }
 }
 
 const clearThings = (...ids) => {
   ids.forEach(id => clearInterval(id))
 }
+
+let startStopBool = true
+let timers = null
+body.addEventListener('keypress', (e) => {
+  if(e.keyCode === 13) {
+    switch(startStopBool) {
+      case true:
+        timers = startThings()
+        break
+      case false:
+        clearThings(timers.timer1, timers.timer2)
+        break
+    }
+    startStopBool = !startStopBool
+  }
+  return null
+})
